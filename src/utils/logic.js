a0cdb9579df6af199e6e914652b6737091710409
Luka Harambasic <luka@harambasic.de>
2020-12-06 22:51:10 +0100

    added comments

--- src/utils/logic.js
+++ src/utils/logic.js
@@ -1,5 +1,9 @@
-// TODO: has to be renamed
-
+// TODO: file has to be renamed
+/**
+ * Set's the initial column
+ * @param files
+ * @returns []
+ */
 export const setup = (files) => {
   console.debug('setup')
   // files is an object, so it's converted to an array to use map()
@@ -13,6 +17,12 @@ export const setup = (files) => {
   })
 }
 
+/**
+ * Add a new column where each item has predfined default states
+ * @param columns
+ * @param children
+ * @returns []
+ */
 export const addColumn = (columns, children) => {
   console.debug('addColumn')
   return [
@@ -26,16 +36,16 @@ export const addColumn = (columns, children) => {
   ]
 }
 
-export const defaultState = (item, isSelected = false, wasSelected = false) => {
-  return {
-    isSelected: isSelected,
-    wasSelected: wasSelected,
-    hasChildren: item.children ? item.children.length > 0 : false
-  }
-}
-
-// tried to make this functional
-// columns could be directly modified and returned
+/**
+ * Set wasSelect for the selected item and reset it for all others. This is relevant if a user navigates back.
+ * Otherwise multiple items would have wasSelected = true
+ * was selected is true if the itemIndex from the clicked item matches the one in the column, all others are false.
+ *
+ * @param columns
+ * @param columnIndex
+ * @param itemIndex
+ * @returns {*}
+ */
 export const setWasSelected = (columns, columnIndex, itemIndex) => {
   console.debug('setWasSelected')
   return columns.map((_column, _columnIndex) => {
@@ -50,6 +60,21 @@ export const setWasSelected = (columns, columnIndex, itemIndex) => {
       return _column
     }
   })
+}
+
+/**
+ * Sets the default states for an item, but also can handle inputs, e.g. setWasSelected()
+ * @param item
+ * @param isSelected
+ * @param wasSelected
+ * @returns {{wasSelected: boolean, hasChildren: boolean, isSelected: boolean}}
+ */
+export const defaultState = (item, isSelected = false, wasSelected = false) => {
+  return {
+    isSelected: isSelected,
+    wasSelected: wasSelected,
+    hasChildren: item.children ? item.children.length > 0 : false
+  }
 }
 
 // maybe relevant for arrow navigation
