99d4864ef4747e8b887ee4ba5ed628bb2e03b429
Luka Harambasic <luka@harambasic.de>
2020-12-24 18:05:28 +0100

    removing unused function

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -17,20 +17,3 @@ export const hasChildren = (item) => item.children ? item.children.length > 0 : 
 export const isSelected = (selectedIds, itemId) => selectedIds[selectedIds.length - 1] === itemId
 
 export const isInPath = (selectedIds, itemId) => selectedIds.includes(itemId)
-
-export const getItemById2 = (tree, id) => {
-  // console.log('getItemById with Id', id)
-  return tree.find(_item => {
-    // console.log('------------------------')
-    // console.log(`itemid: ${_item.id}, searchid: ${id}`)
-    if (_item.id === id) {
-      // console.log('true')
-      return true
-    } else if (hasChildren) {
-      // console.log('hasChildren')
-      return getItemById(_item.children, id)
-    }
-    // console.log('false')
-    return false
-  })
-}
