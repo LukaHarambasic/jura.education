c98d7c92bd8ea42282dcbf5778ec8805e5f2fa5b
Luka Harambasic <luka@harambasic.de>
2020-12-26 22:17:41 +0100

    manifest update - test

--- vue.config.js
+++ vue.config.js
@@ -12,6 +12,23 @@ module.exports = {
     msTileColor: '#50223C',
     orientation: 'landscape',
     assetsVersion: '2',
+    icons: [
+      {
+        src: './img/icons/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: './img/icons/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: './img/icons/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png'
+      }
+    ],
     iconPaths: {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
