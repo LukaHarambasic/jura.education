fbbc6d64cbadd954cc876ab9abe102efd0aecc7c
Luka Harambasic <luka@harambasic.de>
2020-12-29 12:54:09 +0100

    imprint & data privacy - closes #2

--- src/views/Impressum.vue
+++ src/views/Impressum.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="impressum">
     <h1>Impressum</h1>
+    <p>Moritz Köhler</p>
+    <p>Gluckstraße 9</p>
+    <p>79104 Freiburg</p>
+    <p>Deutschland</p>
   </div>
 </template>
 
